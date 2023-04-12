0387f74515df1f7a3078235fd75512f36c3b53c5
mheuangsayaseng <maggieheuang@gmail.com>
2023-04-12 00:47:20 -0500

    Updated Folders

--- src/Finder.jsx
+++ src/Finder.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
 const Finder = (props) => {
+    
+    const [pokemonName, setPokemonName] = useState("");
+
     return (
         <div className="finder">
             <h1>Search for your favorite Puppy!</h1>
+            <input type="text" onChange={(event) => {setPokemonName(event.target.value)}}/>
+            <button>Search Player</button>
         </div>
     );
 };
--- src/Roster.jsx
+++ src/Roster.jsx
@@ -1,4 +1,8 @@
 const Roster = (props) => {
+  
+  const cohortName = "2301-FTB-ET-WEB-AM";
+  const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
+
     return (
       <div className="roster">
         <h1>Check out all the Puppies!</h1>
