a29fb70b7eeb240227491ab866429533b74f1185
mheuangsayaseng <maggieheuang@gmail.com>
2023-04-16 10:49:55 -0500

    Fixed Buttons

--- src/components/NewDogForm.jsx
+++ src/components/NewDogForm.jsx
@@ -21,9 +21,9 @@ export default function NewDogForm() {
                     <input type="text" onChange={(e) => setNewName(e.target.value)}/>
                     <label>Breed:</label>
                     <input type="text" onChange={(e) => setNewBreed(e.target.value)}/>
+                    <br></br>
+                    <button id="joinTeam" style={{width:'150px', marginTop:'2rem'}}>Let's Go!</button>
                 </form>
-                <br></br>
-                <button id="joinTeam" style={{width:'150px'}}>Let's Go!</button>
             </div>
         </div>
     )
